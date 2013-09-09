dc60225261ee4a13a48445afe3c2f0676de5313a
carlqt <carlwilliam.tablante@gmail.com>
2013-09-09 10:33:24 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Goforums::Application.config.secret_key_base = '12c91335ecdd49ea2b384997e5de8f99afb64baf02c17ed17bb2bde5bce98c1aa5a2c9e90bd6b2923ec55294bba15bbd7233ada4156121e79219a4562008772b'
